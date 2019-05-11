1b86165f944c0be6427c11144ea452d135f0d233
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2019-05-11 08:31:52 +0300

    SSDP: unique USN

--- src/discovery_server.py
+++ src/discovery_server.py
@@ -6,24 +6,34 @@ import uuid
 
 import logger
 
+
+def lf_to_crlf(data: str) -> str:
+    return CRLF.join(data.split('\n'))
+
+
+CRLF = '\r\n'
 TIMEOUT = 1.0
 BUFFER_SIZE = 1024 * 2
 M_SEARCH = b'M-SEARCH'
 NOTIFY = b'NOTIFY'
 SERVICE_NAME = 'mdmt2'
-UUID = uuid.uuid3(uuid.NAMESPACE_DNS, SERVICE_NAME)
-REPLY = 'HTTP/1.1 200 OK\r\nCACHE-CONTROL:max-age=5000\r\n' \
-        'ST:upnp:rootdevice\r\nURI:{service}\r\nUSN:uuid:{uuid}\r\nEXT:\r\n'.format(
-            service=SERVICE_NAME, uuid=UUID)
-REPLY += 'Server:{server}\r\nLocation:http://{xml}/\r\nAL:{location}\r\n\r\n'
+UUID = uuid.uuid3(uuid.UUID(int=uuid.getnode(), version=3), SERVICE_NAME)
 SERVER_PORT = 7999
 
-HTTP_HEADER_REPLY = '\r\n'.join('''HTTP/1.1 200 OK
-Content-type: application/xml
-Content-Length: {}
+REPLY = lf_to_crlf('''HTTP/1.1 200 OK
+CACHE-CONTROL:max-age=5000
+ST:upnp:rootdevice
+URI:{service}
+USN:uuid:{uuid}
+EXT:''').format(service=SERVICE_NAME, uuid=UUID) + CRLF
+REPLY += lf_to_crlf('''Server:{server}
+Location:http://{xml_url}/
+AL:{location}''') + CRLF * 2
 
-'''.split('\n'))
-HTTP_XML_REPLY = '\r\n'.join('''<root>
+HTTP_HEADER_REPLY = lf_to_crlf('''HTTP/1.1 200 OK
+Content-type: application/xml
+Content-Length: {}''') + CRLF * 2
+HTTP_XML_REPLY = lf_to_crlf('''<root>
     <specVersion>
         <major>1</major>
         <minor>0</minor>
@@ -31,7 +41,6 @@ HTTP_XML_REPLY = '\r\n'.join('''<root>
     <device>
         <deviceType>urn:schemas-upnp-org:device:MediaRenderer:1</deviceType>
         <friendlyName>mdmTerminal2</friendlyName>
-        <modelDescription>Voice terminal</modelDescription>
         <modelName>mdmTerminal2</modelName>
         <UDN>uuid:{0}</UDN>
         <modelNumber>{1}</modelNumber>
@@ -45,7 +54,20 @@ HTTP_XML_REPLY = '\r\n'.join('''<root>
             </service>
         </serviceList>
     </device>
-</root>'''.split('\n'))
+</root>''')
+
+
+def server_init(sock: socket.socket, address: tuple):
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.settimeout(TIMEOUT)
+    try:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    except AttributeError:
+        pass
+    except socket.error as e:
+        if e.errno != errno.ENOPROTOOPT:
+            raise
+    sock.bind(address)
 
 
 class DiscoveryServer(threading.Thread):
@@ -56,7 +78,6 @@ class DiscoveryServer(threading.Thread):
         self._address = (ip, port)
         self._work = False
         self._server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        self._server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         multicast_req = struct.pack('4sl', socket.inet_aton(multicast_group), socket.INADDR_ANY)
         self._server.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, multicast_req)
@@ -64,15 +85,7 @@ class DiscoveryServer(threading.Thread):
 
     def start(self):
         try:
-            try:
-                self._server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-            except AttributeError:
-                pass
-            except socket.error as e:
-                if e.errno != errno.ENOPROTOOPT:
-                    raise
-            self._server.bind(self._address)
-            self._server.settimeout(TIMEOUT)
+            server_init(self._server, self._address)
         except Exception as e:
             self.log('UDP binding error: {}'.format(e), logger.ERROR)
         else:
@@ -91,6 +104,13 @@ class DiscoveryServer(threading.Thread):
             self.log('stop.', logger.INFO)
 
     def run(self):
+        def make_reply() -> bytes:
+            ip = self.cfg.gts('ip')
+            server = 'mdmTerminal2 version {}; uptime {} seconds'.format(self.cfg.version_str, self.cfg.uptime)
+            location = '{}:{}'.format(ip, SERVER_PORT)
+            xml_url = '{}:{}'.format(ip, self._address[1])
+            return REPLY.format(server=server, xml_url=xml_url, location=location).encode()
+
         while self._work:
             try:
                 msg, address = self._server.recvfrom(BUFFER_SIZE)
@@ -100,13 +120,8 @@ class DiscoveryServer(threading.Thread):
                 self.log('UDP socket error: {}'.format(e), logger.ERROR)
                 continue
             if msg.startswith(M_SEARCH):
-                ip = self.cfg.gts('ip')
-                server = 'mdmTerminal2 version {}; uptime {} seconds'.format(self.cfg.version_str, self.cfg.uptime)
-                location = '{}:{}'.format(ip, SERVER_PORT)
-                xml = '{}:{}'.format(ip, self._address[1])
-                reply = REPLY.format(server=server, xml=xml, location=location).encode()
                 try:
-                    self._server.sendto(reply, address)
+                    self._server.sendto(make_reply(), address)
                 except Exception as e:
                     self.log('UDP reply sending error to {}:{}: {}'.format(*address, e), logger.WARN)
             elif not msg.startswith(NOTIFY):
@@ -123,17 +138,8 @@ class UPNPServer(threading.Thread):
         self._work = False
 
     def start(self):
-        self._server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._server.settimeout(TIMEOUT)
         try:
-            try:
-                self._server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-            except AttributeError:
-                pass
-            except socket.error as e:
-                if e.errno != errno.ENOPROTOOPT:
-                    raise
-            self._server.bind(self._address)
+            server_init(self._server, self._address)
         except Exception as e:
             self.log('TCP binding error {}:{}: {}'.format(*self._address, e), logger.ERROR)
         else:
@@ -148,6 +154,10 @@ class UPNPServer(threading.Thread):
             self._server.close()
 
     def run(self):
+        def make_reply() -> bytes:
+            data = HTTP_XML_REPLY.format(UUID, self.cfg.version_str, self.cfg.gts('ip'), SERVER_PORT).encode()
+            return HTTP_HEADER_REPLY.format(len(data)).encode() + data
+
         def read() -> bytes:
             try:
                 return conn.recv(128)
@@ -155,9 +165,8 @@ class UPNPServer(threading.Thread):
                 return b''
 
         def send_xml():
-            data = HTTP_XML_REPLY.format(UUID, self.cfg.version_str, self.cfg.gts('ip'), SERVER_PORT).encode()
             try:
-                conn.sendall(HTTP_HEADER_REPLY.format(len(data)).encode() + data)
+                conn.sendall(make_reply())
             except socket.timeout:
                 pass
             except Exception as e:
