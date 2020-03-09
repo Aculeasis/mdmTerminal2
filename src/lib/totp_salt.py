81582e27a587f7870f06a129eeae68e62eb14f6d
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2020-03-09 07:23:32 +0300

    Fix authorization.totp -_-

--- src/lib/totp_salt.py
+++ src/lib/totp_salt.py
@@ -1,10 +1,12 @@
 import hashlib
 
 
-def check_token_with_totp(token: str, hash_: str, time_, interval=2, drift=2):
+def check_token_with_totp(token: str, hash_: str, time_, interval=2, drift=2) -> bool:
     def check(salt: int) -> bool:
         return hashlib.sha512((token + str(salt)).encode()).hexdigest() == hash_
-    return check(_salt(interval, time_)) or _salt(interval, time_ - drift) or _salt(interval, time_ + drift)
+    return check(_salt(interval, time_)) or \
+           check(_salt(interval, time_ - drift)) or \
+           check(_salt(interval, time_ + drift))
 
 
 def _salt(interval: int, time_) -> int:
